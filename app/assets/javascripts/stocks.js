6994fd2949679dc5b2549d82d1f70503827f971a
R de Jonge <rene@ikbenfrits.nl>
2017-07-27 15:15:56 +0200

    spinner aanwezig maar verschijen, verdwijnen

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -4,21 +4,25 @@ init_stock_lookup = function() {
 
   $('#stock-lookup-form').on('ajax:before', function(event,data,status){
     show_spinner();
+    document.write("ajax:before");
   });
 
   $('#stock-lookup-form').on('ajax:after', function(event,data,status){
     hide_spinner();
+    document.write("ajax:after");
   });
   
   $('#stock-lookup-form').on('ajax:success', function(event,data,status){
     $('#stock-lookup').replaceWith(data);
     init_stock_lookup();
+    document.write("ajax:success");
   });
   
   $('#stock-lookup-form').on('ajax:error', function(event,xhr,status,error){
     hide_spinner();
     $('#stock-lookup-results').replaceWith(' ');
     $('#stock-lookup-errors').replaceWith('Stock was not found');
+    document.write("ajax:error");
   });
 
 }
